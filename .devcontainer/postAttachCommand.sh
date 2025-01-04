0afc6dd073390dfdcf36b5df77ddd8e36976f04f
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2025-01-04 07:39:46 +0000

    feat: devcontainer設定の更新

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdocs serve --dev-addr=0.0.0.0:8080
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+sudo git config --system --add safe.directory ${containerWorkspaceFolder}
+sudo chown vscode node_modules .pnpm-store
 
 pip3 install --no-cache-dir -r requirements.txt
 
-pnpm install  --frozen-lockfile --prefer-frozen-lockfile
+pnpm install --frozen-lockfile --prefer-frozen-lockfile
